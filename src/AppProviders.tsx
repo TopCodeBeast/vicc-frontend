71da17c4333abbedecdbcce7af3ab97d8c432bc1
TopCodeBeast <maxb47163@gmail.com>
2023-07-05 04:03:04 +0300

    update provider

--- src/AppProviders.tsx
+++ src/AppProviders.tsx
@@ -1,14 +1,24 @@
 import { ReactNode, Suspense, useEffect } from 'react';
 import { BrowserRouter, useLocation } from 'react-router-dom';
 
+import { SO5_SENTRY_DSN } from '@sorare/core/src/config';
+import ConfigProvider from '@sorare/core/src/contexts/config/Provider';
 import { IntlProvider } from '@sorare/core/src/contexts/intl/Provider';
+import SentryProvider from '@sorare/core/src/contexts/sentry/Provider';
+import SeoProvider from '@sorare/core/src/contexts/seo/Provider';
 
 const AppProviders = ({ children }: { children: ReactNode }) => {
   return (
     <BrowserRouter>
-      <IntlProvider>
-        <Suspense fallback={null}>{children}</Suspense>
-      </IntlProvider>
+      <SentryProvider dsn={SO5_SENTRY_DSN}>
+        <IntlProvider>
+          <ConfigProvider>
+            <SeoProvider>
+              <Suspense fallback={null}>{children}</Suspense>
+            </SeoProvider>
+          </ConfigProvider>
+        </IntlProvider>
+      </SentryProvider>
     </BrowserRouter>
   );
 };
