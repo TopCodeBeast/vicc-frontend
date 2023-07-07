5798b9e3f608c4da98ca78cb4dce77a87f0b0c96
TopCodeBeast <maxb47163@gmail.com>
2023-07-07 06:28:58 +0300

    add FlexCard

---
+++ packages/core/src/components/card/CardImg.tsx
@@ -0,0 +1,62 @@
+import styled, { css } from 'styled-components';
+
+import cardPlaceholder from '@core/assets/card_placeholder.svg';
+import ResponsiveImg, {
+  ValidWidths,
+  getClosestStandardWidth,
+} from '@core/atoms/ui/ResponsiveImg';
+
+const WIDTH = 21;
+const HEIGHT = 34;
+
+export const placeholderCss = css`
+  position: absolute;
+  background: var(--c-neutral-300) url(${cardPlaceholder}) no-repeat center;
+  background-size: cover;
+  background-position: top center;
+  border-radius: var(--unit);
+  content: '';
+  inset: 0;
+  z-index: -1;
+`;
+
+type CardImgProps = {
+  width?: ValidWidths;
+  height?: number;
+  fallback?: string;
+  alt?: string;
+};
+export const CardImg = styled(ResponsiveImg).attrs<CardImgProps>(
+  ({ width, height, fallback, alt }) => ({
+    width: width || WIDTH,
+    height: height || (HEIGHT / WIDTH) * +(width || WIDTH),
+    cropWidth: getClosestStandardWidth(width || WIDTH),
+    fallback: fallback ?? cardPlaceholder,
+    alt: alt ?? '',
+  })
+)<CardImgProps>`
+  height: auto;
+  width: 100%;
+`;
+
+export const CardImgLoadingWrapper = styled.div<{ loaded: boolean }>`
+  position: relative;
+  &::before {
+    ${placeholderCss}
+    opacity: ${props => (props.loaded ? '0' : '1')};
+    pointer-events: none;
+    transition: opacity 0.1s ease-out;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    .dark-theme & {
+      background: linear-gradient(
+          180deg,
+          rgba(255, 255, 255, 0.2) 0%,
+          rgba(0, 0, 0, 0) 100%
+        ),
+        var(--c-neutral-200);
+    }
+  }
+`;
---
+++ packages/marketplace/src/components/token/FlexToken/index.tsx
@@ -0,0 +1,84 @@
+import { gql } from '@apollo/client';
+import { ForwardRefExoticComponent, RefAttributes, forwardRef } from 'react';
+import styled from 'styled-components';
+
+import { ValidWidths } from '@sorare/core/src/atoms/ui/ResponsiveImg';
+import OpenItemDialogLink from '@sorare/core/src/components/link/OpenItemDialogLink';
+import UninteractiveToken, {
+  DraggableProps,
+} from '@sorare/core/src/components/token/UninteractiveToken';
+
+// import { useMarketplaceContext } from '@marketplace/contexts/Marketplace';
+
+import { FlexToken_token } from './__generated__/index.graphql';
+
+export type FlexTokenDraggableProps = DraggableProps;
+
+interface Token extends Omit<FlexToken_token, '__typename'> {
+  __typename: 'Card' | 'Token' | 'BaseballCard' | 'NBACard';
+}
+
+export interface FlexTokenProps {
+  token: Token;
+  withLink?: boolean;
+  draggableProps?: FlexTokenDraggableProps;
+  width?: ValidWidths;
+}
+
+const Root = styled(OpenItemDialogLink)`
+  width: 100%;
+  display: block;
+  background-color: transparent;
+  position: relative;
+`;
+
+export const FlexToken: {
+  fragments?: any;
+} & ForwardRefExoticComponent<FlexTokenProps & RefAttributes<HTMLDivElement>> =
+  forwardRef<HTMLDivElement, FlexTokenProps>((props, ref) => {
+    const { token, withLink, draggableProps, width } = props;
+    // const { trackClickCard } = useMarketplaceContext() || {
+    //   trackClickCard: () => {},
+    // };
+
+    if (withLink)
+      return (
+        <Root
+          item={token}
+          onClick={() => console.log(`trackClickCard(token.assetId, token.sport)`)}
+          sport={token.sport}
+        >
+          <UninteractiveToken
+            token={token}
+            draggableProps={draggableProps}
+            ref={ref}
+            width={width}
+          />
+        </Root>
+      );
+    return (
+      <Root as="div">
+        <UninteractiveToken
+          token={token}
+          draggableProps={draggableProps}
+          ref={ref}
+          width={width}
+        />
+      </Root>
+    );
+  });
+FlexToken.displayName = 'FlexToken';
+
+FlexToken.fragments = {
+  token: gql`
+    fragment FlexToken_token on Token {
+      assetId
+      slug
+      sport
+      ...UninteractiveToken_token
+    }
+    ${UninteractiveToken.fragments.token!}
+  `,
+};
+
+export default FlexToken;
