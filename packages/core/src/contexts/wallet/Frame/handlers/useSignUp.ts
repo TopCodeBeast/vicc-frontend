96187a7ce27ae7f1c42a789ffc038e30eeddd1eb
TopCodeBeast <maxb47163@gmail.com>
2023-07-11 17:49:29 +0300

    fix: signup

--- packages/core/src/contexts/wallet/Frame/handlers/useSignUp.ts
+++ packages/core/src/contexts/wallet/Frame/handlers/useSignUp.ts
@@ -2,7 +2,7 @@ import isMobileWeb from 'is-mobile';
 import { useContext, useEffect, useState } from 'react';
 
 import { MessagingContext, SignUp } from '@sorare/wallet-shared';
-// import { SignupPlatform } from '__generated__/globalTypes';
+import { SignupPlatform } from '__generated__/globalTypes';
 import { AcceptTermsInfo, useConnectionContext } from '@core/contexts/connection';
 import { useCurrentUserContext } from '@core/contexts/currentUser';
 import useSignUp from '@core/hooks/auth/useSignUp';
@@ -34,6 +34,7 @@ export default () => {
   useEffect(
     () =>
       registerHandler<SignUp>('signUp', async args => {
+        console.log('signup-args~~~~~~~~~~~~~~~~', args)
         const { version, mobile, ...rest } = args;
 
         // Terms and Conditions Modal
@@ -52,33 +53,32 @@ export default () => {
 
         setAcceptedTerms(acceptTerms);
 
-        //TODO*****************
-        // let signupPlatform: SignupPlatform = SignupPlatform.WEB;
-        // if (mobile) signupPlatform = SignupPlatform.MOBILE;
-        // else if (isMobileWeb()) signupPlatform = SignupPlatform.MOBILE_WEB;
+        let signupPlatform: SignupPlatform = SignupPlatform.WEB;
+        if (mobile) signupPlatform = SignupPlatform.MOBILE;
+        else if (isMobileWeb()) signupPlatform = SignupPlatform.MOBILE_WEB;
 
-        // const { data } = await signUp({
-        //   ...rest,
-        //   signupPlatform,
-        //   certified: 'certified',
-        //   ...acceptTerms,
-        // });
+        const { data } = await signUp({
+          ...rest,
+          signupPlatform,
+          certified: 'certified',
+          ...acceptTerms,
+        });
 
-        // if (data!.signUp) {
-        //   const { errors } = data!.signUp;
-        //   if (errors && errors.length !== 0) {
-        //     return {
-        //       error: errors.reduce<Record<string, string>>((sum, err) => {
-        //         if (err.path?.length !== 2) return sum;
-        //         sum[err.path[1]] = err.message;
-        //         return sum;
-        //       }, {}),
-        //     };
-        //   }
-        //   refetch();
-        // }
-        // setAcceptedTerms(null);
-        // closeConnectionDialog(rest.email);
+        if (data!.signUp) {
+          const { errors } = data!.signUp;
+          if (errors && errors.length !== 0) {
+            return {
+              error: errors.reduce<Record<string, string>>((sum, err) => {
+                if (err.path?.length !== 2) return sum;
+                sum[err.path[1]] = err.message;
+                return sum;
+              }, {}),
+            };
+          }
+          refetch();
+        }
+        setAcceptedTerms(null);
+        closeConnectionDialog(rest.email);
         return { error: {} };
       }),
     [
