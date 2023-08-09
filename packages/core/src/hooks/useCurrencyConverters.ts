6c8938063a4b76a8affa994b81c7769201b1bc9e
TopCodeBeast <maxb47163@gmail.com>
2023-08-09 10:32:56 +0300

    fix: exchange rate - todo

--- packages/core/src/hooks/useCurrencyConverters.ts
+++ packages/core/src/hooks/useCurrencyConverters.ts
@@ -11,44 +11,48 @@ export default () => {
 
   const convertFromWei = useCallback(
     (amountInWei: string | number, currencyCode: CurrencyCode) =>
-      exchangeRate?.rates
-        ? new Big(amountInWei)
-            .multipliedBy(
-              asObject(exchangeRate?.rates).wei[currencyCode.toLowerCase()]
-            )
-            .toNumber()
-        : 0,
+      0,
+      // exchangeRate?.rates
+      //   ? new Big(amountInWei)
+      //       .multipliedBy(
+      //         asObject(exchangeRate?.rates).wei[currencyCode.toLowerCase()]
+      //       )
+      //       .toNumber()
+      //   : 0,
     [exchangeRate?.rates]
   );
 
   const convertFromEth = useCallback(
     (amountInEth: string | number, currencyCode: CurrencyCode) =>
-      new Big(amountInEth)
-        .multipliedBy(
-          asObject(exchangeRate.rates).eth[currencyCode.toLowerCase()]
-        )
-        .toNumber(),
+      0,
+      // new Big(amountInEth)
+      //   .multipliedBy(
+      //     asObject(exchangeRate.rates).eth[currencyCode.toLowerCase()]
+      //   )
+      //   .toNumber(),
     [exchangeRate?.rates]
   );
 
   const convertToEth = useCallback(
     (amountInEur: string, currencyCode: CurrencyCode) =>
-      new Big(
-        Number.parseFloat(amountInEur) /
-          Number.parseFloat(
-            asObject(exchangeRate.rates).eth[currencyCode.toLowerCase()]
-          )
-      )
-        .decimalPlaces(ETH_DECIMAL_PLACES)
-        .toNumber(),
+      0,
+      // new Big(
+      //   Number.parseFloat(amountInEur) /
+      //     Number.parseFloat(
+      //       asObject(exchangeRate.rates).eth[currencyCode.toLowerCase()]
+      //     )
+      // )
+      //   .decimalPlaces(ETH_DECIMAL_PLACES)
+      //   .toNumber(),
     [exchangeRate?.rates]
   );
 
   const convertToWei = useCallback(
     (amountInEur: string, currencyCode: CurrencyCode) =>
-      new Big(amountInEur)
-        .dividedBy(asObject(exchangeRate.rates).wei[currencyCode.toLowerCase()])
-        .decimalPlaces(0),
+      0,
+      // new Big(amountInEur)
+      //   .dividedBy(asObject(exchangeRate.rates).wei[currencyCode.toLowerCase()])
+      //   .decimalPlaces(0),
     [exchangeRate?.rates]
   );
 
