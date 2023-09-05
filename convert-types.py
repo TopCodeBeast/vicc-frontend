51bac963e1b5263dd25bc2d70f70636bbd0811fb
TopCodeBeast <maxb47163@gmail.com>
2023-09-05 12:30:45 +0000

    update script

--- convert-types.py
+++ convert-types.py
@@ -12,37 +12,54 @@ from shutil import copytree
 
 classNameList = []
 
+def replace(text, oldvalue, newvalue, startIndex, endIndex):
+    i = 0
+    while i >= 0:
+        if endIndex >= 0: i = text.find(oldvalue, startIndex, endIndex)
+        else: i = text.find(oldvalue, startIndex)
+        if i >= 0:
+            text = text[:i] + newvalue + text[i + len(oldvalue):]
+    return text
+
+
 def updateTypes(filePath):
     print(filePath)
     text = ''
     with open(filePath, 'r', encoding='utf-8') as fp:
         text = fp.read()
 
-    updated = text
+    code = text
+    code = code.replace("LAST_FIVE_SO5_AVERAGE_SCORE", "LAST_FIVE_VICC5_AVERAGE_SCORE")
+    code = code.replace("LAST_FIFTEEN_SO5_AVERAGE_SCORE", "LAST_FIFTEEN_VICC5_AVERAGE_SCORE")
 
-    updated = updated.replace("LAST_FIVE_SO5_AVERAGE_SCORE", "LAST_FIVE_VICC5_AVERAGE_SCORE")
-    updated = updated.replace("LAST_FIFTEEN_SO5_AVERAGE_SCORE", "LAST_FIFTEEN_VICC5_AVERAGE_SCORE")
-
-    updated = updated.replace("sport: FOOTBALL", "sport: CRICKET")
-    updated = updated.replace("TokenFootballMetadata", "TokenCricketMetadata")
-    updated = updated.replace("sorarePrivateKeyRecovery", "viccPrivateKeyRecovery")
-    updated = updated.replace("sorarePrivateKey", "viccPrivateKey")
-    updated = updated.replace("sorareEncryptionKey", "viccEncryptionKey")
-    updated = updated.replace("sorareTokensAddress", "viccTokensAddress")
-    updated = updated.replace("sorareCardsAddress", "viccCardsAddress")
-    updated = updated.replace("sorareAddress", "viccAddress")
-    updated = updated.replace("sorareManaged", "viccManaged")
+    code = code.replace("sport: FOOTBALL", "sport: CRICKET")
+    code = code.replace("TokenFootballMetadata", "TokenCricketMetadata")
+    code = code.replace("sorarePrivateKeyRecovery", "viccPrivateKeyRecovery")
+    code = code.replace("sorarePrivateKey", "viccPrivateKey")
+    code = code.replace("sorareEncryptionKey", "viccEncryptionKey")
+    code = code.replace("sorareTokensAddress", "viccTokensAddress")
+    code = code.replace("sorareCardsAddress", "viccCardsAddress")
+    code = code.replace("sorareAddress", "viccAddress")
+    code = code.replace("sorareManaged", "viccManaged")
     
-    updated = updated.replace("FOOTBALL = 'FOOTBALL'", "CRICKET = 'CRICKET'")
-    updated = updated.replace("Sport.FOOTBALL", "Sport.CRICKET")
-    updated = updated.replace("footballCardCollections", "cricketCardCollections")
-    updated = updated.replace("footballProfile", "cricketProfile")
-    updated = updated.replace("footballLast30DaysLineupsCount", "cricketLast30DaysLineupsCount")
-    updated = updated.replace("footballNationalSeriesTokensAddress", "cricketNationalSeriesTokensAddress")
+    code = code.replace("FOOTBALL = 'FOOTBALL'", "CRICKET = 'CRICKET'")
+    code = code.replace("Sport.FOOTBALL", "Sport.CRICKET")
+    code = code.replace("footballCardCollections", "cricketCardCollections")
+    code = code.replace("footballProfile", "cricketProfile")
+    code = code.replace("footballLast30DaysLineupsCount", "cricketLast30DaysLineupsCount")
+    code = code.replace("footballNationalSeriesTokensAddress", "cricketNationalSeriesTokensAddress")
 
-    if text != updated:
+    startIndex = 0
+    while True:
+        endIndex = code.find(" from '", startIndex)
+        code = replace(code, "so5", "vicc5", startIndex, endIndex)
+        code = replace(code, "So5", "Vicc5", startIndex, endIndex)
+        if endIndex < 0: break
+        startIndex = code.index("'", code.index("'", endIndex) + 1)
+    
+    if code != text:
         with open(filePath, 'w+', encoding='utf-8') as fp:
-            fp.write(updated)
+            fp.write(code)
 
 if __name__ == '__main__':
     src = './packages'
